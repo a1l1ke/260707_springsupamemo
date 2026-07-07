47b7760329ad0fc8e689c353e84c7fd2a86e397d
a1l1ke <onetenthplease@gmail.com>
2026-07-07 15:27:13 +0900

    refactor: MemoRepository에 SupabaseUtil 의존성 주입 및 save/findAll 연동

--- src/main/java/org/example/springsupamemo/repository/MemoRepository.java
+++ src/main/java/org/example/springsupamemo/repository/MemoRepository.java
@@ -1,17 +1,22 @@
 package org.example.springsupamemo.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.example.springsupamemo.model.MemoEntity;
+import org.example.springsupamemo.util.SupabaseUtil;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemoRepository {
-    public void save(MemoEntity memoEntity) {
+    private final SupabaseUtil supabaseUtil;
 
+    public void save(MemoEntity memoEntity) {
+        supabaseUtil.save(memoEntity);
     }
 
     public List<MemoEntity> findAll() {
-        return List.of();
+        return supabaseUtil.getAll();
     }
 }

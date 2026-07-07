d631c1c3ab179217d272b5f258ec86109ad045f1
a1l1ke <onetenthplease@gmail.com>
2026-07-07 14:31:31 +0900

    feat: HomeController에 POST 요청 처리 및 세션 기반 메모 저장 로직 추가

--- src/main/java/org/example/springsupamemo/controller/HomeController.java
+++ src/main/java/org/example/springsupamemo/controller/HomeController.java
@@ -1,8 +1,16 @@
 package org.example.springsupamemo.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.springsupamemo.dto.MemoFormDTO;
+import org.example.springsupamemo.dto.MemoViewDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping // '/'
@@ -10,5 +18,18 @@ public class HomeController {
     @GetMapping
     public String home() {
         return "home";
+    }
+
+    @PostMapping
+    public String memoForm(
+            @ModelAttribute MemoFormDTO memoFormDTO,
+            HttpSession session) {
+        List<MemoViewDTO> memoList = (List<MemoViewDTO>) session.getAttribute("memoList");
+        if (memoList == null) {
+            memoList = new ArrayList<>();
+        }
+        memoList.add(new MemoViewDTO(memoFormDTO.memo()));
+        session.setAttribute("memoList", memoList);
+        return "redirect:/"; // GET / -> home
     }
 }
